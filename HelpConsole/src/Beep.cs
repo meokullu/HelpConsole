3c3ed9eeb43cfc9a46dc93fd051cf30662a29c67
Muhammed Enes Okullu <meokullu@gmail.com>
2023-10-26 20:03:08 +0300

    v1.0.0-beta

--- HelpConsole/src/Beep.cs
+++ HelpConsole/src/Beep.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace HelpConsole
 {
@@ -12,37 +8,79 @@ namespace HelpConsole
     /// </summary>
     public partial class HelpConsole
     {
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        //public class Melody
-        //{
-        //    /// <summary>
-        //    /// 
-        //    /// </summary>
-        //    public int[] Notes;
-        //}
+        /// <summary>
+        /// Beep for warning.
+        /// </summary>
+        public static Melody Warning = new Melody { Notes = new int[]{ 250, 250, 0 } };
 
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        ///// <param name="melody"></param>
-        //public static void Beep(Melody melody)
-        //{
-        //    // Checking if melody is null.
-        //    if (melody == null)
-        //    {
-        //        // Throwing null exception to indicate provided value is null.
-        //        throw new ArgumentNullException("Melody is null");
-        //    }
+        /// <summary>
+        /// Beep for welcome.
+        /// </summary>
+        public static Melody Welcome = new Melody { Notes = new int[] { 2000, 400, 25, 1750, 400, 25, 1650, 400, 25 } };
 
-        //    // Checking if melody is null.
-        //    if (melody.Notes.Length == 0)
-        //    {
-        //        // Throwing null exception to indicate provided value is null.
-        //        throw new ArgumentNullException("Melody content is null");
-        //    }
-        //}
+        /// <summary>
+        /// Beep for success.
+        /// </summary>
+        public static Melody Success = new Melody { Notes = new int[] { 750, 200, 100, 650, 250, 100, 850, 150, 65, 450, 450, 0 } };
+
+        /// <summary>
+        /// Melody holds integer array with an order of sound frequency, duuration of sound and waiting.
+        /// </summary>
+        public class Melody
+        {
+            /// <summary>
+            /// Pairs of (frequency between 38 and 32767, duration of beep, waiting).
+            /// </summary>
+            public int[] Notes;
+        }
+
+        /// <summary>
+        /// Plays provided melody.
+        /// </summary>
+        /// <param name="melody">Array of frequency, duration of beeping and waiting list.</param>
+        public static void Beep(Melody melody)
+        {
+            // Checking if melody is null.
+            if (melody == null)
+            {
+                // Throwing null exception to indicate provided value is null.
+                throw new ArgumentNullException("Melody is null");
+            }
+
+            // Checking if melody is null.
+            if (melody.Notes.Length == 0)
+            {
+                // Throwing null exception to indicate provided value is null.
+                throw new ArgumentNullException("Melody content is null");
+            }
+
+            // Loop for notes array.
+            for (int i = 0; i < melody.Notes.Length; i = i + 3)
+            {
+                // Checking if frequency of note is in range, it
+                if (melody.Notes[i] < 37 || melody.Notes[i] > 32767 || melody.Notes[i + 1] < 0 || melody.Notes[i + 2] < 0 )
+                {
+                    //
+                    Console.WriteLine("Error on notes");
+
+                    //
+                    continue;
+                }
+
+                //
+                Console.Beep(melody.Notes[i], melody.Notes[i + 1]);
+
+                //
+                if (melody.Notes[i + 2] != 0)
+                {
+                    //
+                    Thread.Sleep(melody.Notes[i + 2]);
+                }
+
+                //
+                Console.WriteLine(i);
+            }
+        }
 
         // Warning beep frequency.
         private const int _warningBeepFrequency = 725;
