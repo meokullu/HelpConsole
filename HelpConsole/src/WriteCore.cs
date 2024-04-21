6b29b7bfdeb0190d2f9b508f225c5a8836ed41b0
Muhammed Enes Okullu <meokullu@gmail.com>
2024-04-21 21:37:11 +0300

    v1.7.0 files splitting.

---
+++ HelpConsole/src/WriteCore.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace HelpConsole
+{
+    /// <summary>
+    /// Calls Console.Write().
+    /// </summary>
+    public partial class HelpConsole
+    {
+        #region Write
+
+        /// <summary>
+        /// Calls <see cref="Console.Write(bool)"/>.
+        /// </summary>
+        /// <param name="value">Value to write.</param>
+        public static void Write(bool value)
+        {
+            Console.Write(value: value);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.Write(char)"/>.
+        /// </summary>
+        /// <param name="value">Value to write.</param>
+        public static void Write(char value)
+        {
+            Console.Write(value: value);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.Write(char[])"/>.
+        /// </summary>
+        /// <param name="buffer">Value to write.</param>
+        public static void Write(char[] buffer)
+        {
+            Console.Write(buffer: buffer);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.Write(char[], int, int)"/>.
+        /// </summary>
+        /// <param name="buffer">Value to write.</param>
+        /// <param name="index">Starting index.</param>
+        /// <param name="count">Count of specified array length.</param>
+        public static void Write(char[] buffer, int index, int count)
+        {
+            Console.Write(buffer: buffer, index: index, count: count);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.Write(double)"/>.
+        /// </summary>
+        /// <param name="value">Value to write.</param>
+        public static void Write(double value)
+        {
+            Console.Write(value: value);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.Write(decimal)"/>.
+        /// </summary>
+        /// <param name="value">Value to write.</param>
+        public static void Write(decimal value)
+        {
+            Console.Write(value: value);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.Write(float)"/>.
+        /// </summary>
+        /// <param name="value">Value to write.</param>
+        public static void Write(float value)
+        {
+            Console.Write(value: value);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.Write(int)"/>.
+        /// </summary>
+        /// <param name="value">Value to write.</param>
+        public static void Write(int value)
+        {
+            Console.Write(value: value);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.Write(uint)"/>.
+        /// </summary>
+        /// <param name="value">Value to write.</param>
+        public static void Write(uint value)
+        {
+            Console.Write(value: value);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.Write(long)"/>.
+        /// </summary>
+        /// <param name="value">Value to write.</param>
+        public static void Write(long value)
+        {
+            Console.Write(value: value);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.Write(ulong)"/>.
+        /// </summary>
+        /// <param name="value">Value to write.</param>
+        public static void Write(ulong value)
+        {
+            Console.Write(value: value);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.Write(object)"/>.
+        /// </summary>
+        /// <param name="value">Value to write.</param>
+        public static void Write(object value)
+        {
+            Console.Write(value: value);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.Write(string)"/>.
+        /// </summary>
+        /// <param name="value">Value to write.</param>
+        public static void Write(string value)
+        {
+            Console.Write(value: value);
+        }
+
+        #endregion Write
+    }
+}
---
+++ HelpConsole/src/WriteLineCore.cs
@@ -0,0 +1,146 @@
+﻿using System;
+
+namespace HelpConsole
+{
+    /// <summary>
+    /// Calls Console.WriteLine().
+    /// </summary>
+    public partial class HelpConsole
+    {
+        #region WriteLine
+
+        /// <summary>
+        /// Calls <see cref="Console.WriteLine(bool)"/>.
+        /// </summary>
+        /// <param name="value">Value to write.</param>
+        public static void WriteLine(bool value)
+        {
+            //
+            Console.WriteLine(value: value);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.WriteLine(char)"/>.
+        /// </summary>
+        /// <param name="value">Value to write.</param>
+        public static void WriteLine(char value)
+        {
+            //
+            Console.WriteLine(value: value);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.WriteLine(char[])"/>.
+        /// </summary>
+        /// <param name="buffer">Value to write.</param>
+        public static void WriteLine(char[] buffer)
+        {
+            //
+            Console.WriteLine(buffer: buffer);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.WriteLine(char[], int, int)"/>.
+        /// </summary>
+        /// <param name="buffer">Value to write.</param>
+        /// <param name="index"></param>
+        /// <param name="count"></param>
+        public static void WriteLine(char[] buffer, int index, int count)
+        {
+            //
+            Console.WriteLine(buffer: buffer, index: index, count: count);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.WriteLine(double)"/>.
+        /// </summary>
+        /// <param name="value">Value to write.</param>
+        public static void WriteLine(double value)
+        {
+            //
+            Console.WriteLine(value: value);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.WriteLine(decimal)"/>.
+        /// </summary>
+        /// <param name="value">Value to write.</param>
+        public static void WriteLine(decimal value)
+        {
+            //
+            Console.WriteLine(value: value);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.WriteLine(float)"/>.
+        /// </summary>
+        /// <param name="value">Value to write.</param>
+        public static void WriteLine(float value)
+        {
+            //
+            Console.WriteLine(value: value);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.WriteLine(int)"/>.
+        /// </summary>
+        /// <param name="value">Value to write.</param>
+        public static void WriteLine(int value)
+        {
+            //
+            Console.WriteLine(value: value);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.WriteLine(uint)"/>.
+        /// </summary>
+        /// <param name="value">Value to write.</param>
+        public static void WriteLine(uint value)
+        {
+            //
+            Console.WriteLine(value: value);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.WriteLine(long)"/>.
+        /// </summary>
+        /// <param name="value">Value to write.</param>
+        public static void WriteLine(long value)
+        {
+            //
+            Console.WriteLine(value: value);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.WriteLine(ulong)"/>.
+        /// </summary>
+        /// <param name="value">Value to write.</param>
+        public static void WriteLine(ulong value)
+        {
+            //
+            Console.WriteLine(value: value);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.WriteLine(object)"/>.
+        /// </summary>
+        /// <param name="value">Value to write.</param>
+        public static void WriteLine(object value)
+        {
+            //
+            Console.WriteLine(value: value);
+        }
+
+        /// <summary>
+        /// Calls <see cref="Console.WriteLine(string)"/>.
+        /// </summary>
+        /// <param name="value">Value to write.</param>
+        public static void WriteLine(string value)
+        {
+            //
+            Console.WriteLine(value: value);
+        }
+
+        #endregion WriteLine
+    }
+}
